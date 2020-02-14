c01d190b36c3823740382a306b460a692f65f461
Lise Savard <lise@lise.me>
2020-02-14 01:39:26 +0000

    Add stub methods & makefile for dpu program

---
+++ snappy/decompress/dpu_decompress.c
@@ -0,0 +1,21 @@
+#include "dpu_decompress.h"
+
+size_t dpu_uncompress(const char* compressed, 
+                      size_t compressed_len,
+                      char* uncompressed,
+                      size_t uncompressed_len) {
+    // TODO: implement this
+    (void) compressed;
+    (void) compressed_len;
+    (void) uncompressed;
+    (void) uncompressed_len;
+    return 0;
+}
+
+size_t dpu_uncompressed_length(const char* compressed, size_t compressed_len) {
+    // TODO: implement this
+    (void) compressed;
+    (void) compressed_len;
+    return 0;
+}
+
---
+++ snappy/decompress/dpu_task.c
@@ -0,0 +1,7 @@
+#include "dpu_decompress.h"
+
+int main() {
+  // TODO
+  return 0;
+}
+
