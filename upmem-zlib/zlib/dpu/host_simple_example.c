eac310b1280b078dfdf88d08ddfa7580075ea4b9
Penjani Chavula <chavulapenjani@gmail.com>
2020-02-21 17:41:22 -0800

    copyin code from zpipe example

--- upmem-zlib/zlib/dpu/host_simple_example.c
+++ upmem-zlib/zlib/dpu/host_simple_example.c
@@ -26,10 +26,52 @@
  */
 int CPU_compress(FILE *source, FILE *dest, int level)
 {
-	// TODO: Implement this
-	fprintf(stdout, "Not implemented yet");
+	int ret, flush;
+	unsigned have;
+	z_stream strm;
+	unsigned char in[CHUNK];
+	unsigned char out[CHUNK];
 
-	return 0;
+	/* allocate deflate state */
+	strm.zalloc = Z_NULL;
+	strm.zfree = Z_NULL;
+	strm.opaque = Z_NULL;
+	ret = deflateInit(&strm, level);
+	if (ret != Z_OK)
+		return ret;
+
+	/* compress until end of file */
+	do {
+		strm.avail_in = fread(in, 1, CHUNK, source);
+		if (ferror(source)) {
+			(void)deflateEnd(&strm);
+			return Z_ERRNO;
+		}
+		flush = feof(source) ? Z_FINISH : Z_NO_FLUSH;
+		strm.next_in = in;
+
+		/* run deflate() on input until output buffer not full, finish
+		   compression if all of source has been read in */
+		do {
+			strm.avail_out = CHUNK;
+			strm.next_out = out;
+			ret = deflate(&strm, flush);    /* no bad return value */
+			assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+			have = CHUNK - strm.avail_out;
+			if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+				(void)deflateEnd(&strm);
+				return Z_ERRNO;
+			}
+		} while (strm.avail_out == 0);
+		assert(strm.avail_in == 0);     /* all input will be used */
+
+		/* done when last data in file processed */
+	} while (flush != Z_FINISH);
+	assert(ret == Z_STREAM_END);        /* stream will be complete */
+
+	/* clean up and return */
+	(void)deflateEnd(&strm);
+	return Z_OK;
 }
 
 /*
@@ -39,6 +81,60 @@ int CPU_decompress(FILE *source, FILE *dest)
 {
 	// TODO: Implement this
 	fprintf(stdout, "Not implemented yet");
+	int ret;
+	unsigned have;
+	z_stream strm;
+	unsigned char in[CHUNK];
+	unsigned char out[CHUNK];
+
+	/* allocate inflate state */
+	strm.zalloc = Z_NULL;
+	strm.zfree = Z_NULL;
+	strm.opaque = Z_NULL;
+	strm.avail_in = 0;
+	strm.next_in = Z_NULL;
+	ret = inflateInit(&strm);
+	if (ret != Z_OK)
+		return ret;
+
+	/* decompress until deflate stream ends or end of file */
+	do {
+		strm.avail_in = fread(in, 1, CHUNK, source);
+		if (ferror(source)) {
+			(void)inflateEnd(&strm);
+			return Z_ERRNO;
+		}
+		if (strm.avail_in == 0)
+			break;
+		strm.next_in = in;
+
+		/* run inflate() on input until output buffer not full */
+		do {
+		    strm.avail_out = CHUNK;
+		    strm.next_out = out;
+		    ret = inflate(&strm, Z_NO_FLUSH);
+		    assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+		    switch (ret) {
+			    case Z_NEED_DICT:
+				ret = Z_DATA_ERROR;     /* and fall through */
+			    case Z_DATA_ERROR:
+			    case Z_MEM_ERROR:
+				(void)inflateEnd(&strm);
+				return ret;
+		    }
+		    have = CHUNK - strm.avail_out;
+		    if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+			(void)inflateEnd(&strm);
+			return Z_ERRNO;
+		    }
+		} while (strm.avail_out == 0);
+
+		/* done when inflate() says it's done */
+	} while (ret != Z_STREAM_END);
+
+	/* clean up and return */
+	(void)inflateEnd(&strm);
+	return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
 
 	return 0;
 }
@@ -92,11 +188,33 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
+	FILE *fin = fopen(argv[3], "r");
+	if (fin == NULL) {
+		fprintf(stderr, "Invalid input file");
+		return EXIT_FAILURE;
+	}
+
+	FILE *fout = fopen(argv[4], "w");
+	if (fout == NULL) {
+		fprintf(stderr, "Invalid output file");
+		return EXIT_FAILURE;
+	}
+
+	int ret;
+
 	if (strcmp(argv[1], "--cpu") == 0) {
 		if (strcmp(argv[2], "--compress") == 0) {
 			// Prepare the cpu compression
+			ret = CPU_compress(fin, fout, Z_DEFAULT_COMPRESSION);
+			if (ret != Z_OK)
+				zerr(ret);
+			return ret;			
 		} else if (strcmp(argv[2], "--decompress") == 0) {
 			// prepare the cpu decompression	
+			ret = CPU_decompress(fin, fout);
+			if (ret != Z_OK)
+				zerr(ret);
+			return ret;
 		} else {
 			fprintf(stderr, "Unknown arguments");
 			fprintf(stderr, usage);
@@ -104,8 +222,16 @@ int main(int argc, char **argv)
 	} else if (strcmp(argv[1], "--dpu") == 0) {
 		if (strcmp(argv[2], "--compress") == 0) {
 			// Prepare the dpu compression
+			ret = DPU_compress(fin, fout, Z_DEFAULT_COMPRESSION);
+			if (ret != Z_OK)
+				zerr(ret);
+			return ret;
 		} else if (strcmp(argv[2], "--decompress") == 0) {
 			// prepare the dpu decompression	
+			ret = DPU_decompress(fin, fout);
+			if (ret != Z_OK)
+				zerr(ret);
+			return ret;
 		} else {
 			fprintf(stderr, "Unknown arguments");
 			fprintf(stderr, usage);
@@ -116,6 +242,5 @@ int main(int argc, char **argv)
 		fprintf(stderr, usage);
 	}	
 
-	fprintf(stdout, "Not implemented yet");
-	return EXIT_SUCCESS;
+	return EXIT_FAILURE;
 }
