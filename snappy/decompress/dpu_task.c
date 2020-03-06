945f511958d08921e5512beb654fc6bb40cf9ab5
Lise Savard <lise@lise.me>
2020-03-06 02:45:20 +0000

    Add simple dpu program

--- snappy/decompress/dpu_task.c
+++ snappy/decompress/dpu_task.c
@@ -1,7 +1,16 @@
+#include <mram.h>
+
 #include "dpu_decompress.h"
 
+#define BUF_SIZE (1 << 10)
+
+__host uint64_t compressed_length;
+__host uint64_t uncompressed_length;
+__host char compressed[BUF_SIZE];
+
 int main() {
-  // TODO
-  return 0;
+    size_t result = dpu_uncompressed_length(compressed, compressed_length);
+    uncompressed_length = result;
+    return 0;
 }
 
