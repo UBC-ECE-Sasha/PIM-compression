c9993cf9cb3be55af327b5268230f4a671945a37
Lise Savard <lise@lise.me>
2020-03-06 05:01:34 +0000

    Implement snappy_uncompressed_length

--- snappy/README.md
+++ snappy/README.md
@@ -8,3 +8,13 @@ Compression is done on the host CPU, decompression on DPU.
 
 `make` to build both the host and DPU programs.
 
+## Test
+
+```
+./decompress_host test/alice.snappy
+```
+
+This should return the uncompressed size of the test file (895).
+
+`alice.snappy` is text file that was originally compressed with snappy.
+
--- snappy/decompress/dpu_decompress.c
+++ snappy/decompress/dpu_decompress.c
@@ -1,4 +1,56 @@
+#include <stdint.h>
+
 #include "dpu_decompress.h"
+
+/**
+ * Attempts to parse a varint32 from a prefix of the bytes in [ptr,limit-1].
+ * Never reads a character at or beyond limit. If a valid/terminated varint32
+ * was found in the range, stores it in *OUTPUT and returns a pointer just
+ * past the last byte of the varint32. Else returns NULL. On success,
+ * "result <= limit".
+ */
+static const char *varint_parse32_with_limit(const char *p, const char *l,
+                                             uint32_t *output) {
+    const unsigned char *ptr = (const unsigned char *)(p);
+    const unsigned char *limit = (const unsigned char *)(l);
+    uint32_t b, result;
+
+    if (ptr >= limit) return NULL;
+    b = *(ptr++);
+    result = b & 127;
+
+    if (b < 128) goto done;
+    if (ptr >= limit) return NULL;
+
+    b = *(ptr++);
+    result |= (b & 127) << 7;
+
+    if (b < 128) goto done;
+    if (ptr >= limit) return NULL;
+
+    b = *(ptr++);
+    result |= (b & 127) << 14;
+
+    if (b < 128) goto done;
+    if (ptr >= limit) return NULL;
+
+    b = *(ptr++);
+    result |= (b & 127) << 21;
+
+    if (b < 128) goto done;
+    if (ptr >= limit) return NULL;
+
+    b = *(ptr++);
+    result |= (b & 127) << 28;
+
+    if (b < 16) goto done;
+
+    return NULL;  /* Value is too long to be a varint32 */
+
+done:
+    *output = result;
+    return (const char *)(ptr);
+}
 
 int dpu_uncompress(const char* compressed, 
                    size_t compressed_len,
@@ -12,10 +64,12 @@ int dpu_uncompress(const char* compressed,
 
 int dpu_uncompressed_length(const char* compressed, size_t compressed_len,
                             size_t *result) {
-    // TODO: implement this
-    (void) compressed;
-    (void) compressed_len;
-    *result = 0;
+    uint32_t v;
+    const char *limit = compressed + compressed_len;
+    if (varint_parse32_with_limit(compressed, limit, &v) == NULL) {
+        return -1;
+    }
+    *result = v;
     return 0;
 }
 
