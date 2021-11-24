178c6b0d6d8c7e325e616662169c333d9dd80e1b
Nima Nasiri <nimanasiri2525@hotmail.com>
2021-11-24 12:08:56 -0800

    Debugging checkpoint

--- snappy/snappy_compress.c
+++ snappy/snappy_compress.c
@@ -16,6 +16,57 @@
 #define MAX_HASH_TABLE_BITS 14
 #define MAX_HASH_TABLE_SIZE (1U << MAX_HASH_TABLE_BITS)
 
+/* Types */
+typedef uint8_t BYTE;
+typedef uint16_t U16;
+typedef uint32_t U32;
+
+/* LZ4 constants */
+#define LZ4_DISTANCE_MAX 65535
+#define LASTLITERALS 	 5
+#define MFLIMIT 		 12
+
+/* Encoding Constants*/
+#define ML_BITS 4
+#define ML_MASK ((1U<<ML_BITS)-1)
+#define RUN_BITS (8-ML_BITS)
+#define RUN_MASK ((1U<<RUN_BITS)-1)
+
+/* Reading and writing to memory */
+
+/**
+ * LZ4 relies on memcpy with a constant size being inlined. In freestanding
+ * environments, the compiler can't assume the implementation of memcpy() is
+ * standard compliant, so it can't apply its specialized memcpy() inlining
+ * logic. When possible, use __builtin_memcpy() to tell the compiler to analyze
+ * memcpy() as if it were standard compliant, so it can inline it in freestanding
+ * environments. This is needed when decompressing the Linux Kernel, for example.
+ */
+#if defined(__GNUC__) && (__GNUC__ >= 4)
+#define LZ4_memcpy(dst, src, size) __builtin_memcpy(dst, src, size)
+#else
+#define LZ4_memcpy(dst, src, size) memcpy(dst, src, size)
+#endif
+
+static unsigned LZ4_isLittleEndian(void)
+{
+    const union { U32 u; BYTE c[4]; } one = { 1 };   /* don't use static : performance detrimental */
+    return one.c[0];
+}
+
+static void LZ4_write16(void* memPtr, U16 value) { *(U16*)memPtr = value; }
+static void LZ4_write32(void* memPtr, U32 value) { *(U32*)memPtr = value; }
+
+static const U32 LZ4_skipTrigger = 6;
+
+/*-************************************
+*  Error detection
+**************************************/
+#  ifndef assert
+#    define assert(condition) ((void)0)
+#  endif
+
+
 /**
  * Calculate the rounded down log base 2 of an unsigned integer.
  *
@@ -27,34 +78,39 @@ static inline int32_t log2_floor(uint32_t n)
 	return (n == 0) ? -1 : 31 ^ __builtin_clz(n);
 }
 
-/**
- * Calculate the maximum expected compressed length for a given
- * uncompressed length.
- *	 
- * Compressed data can be defined as:
- *	  compressed := item* literal*
- *	  item		 := literal* copy
- *
- * The trailing literal sequence has a space blowup of at most 62/60
- * since a literal of length 60 needs one tag byte + one extra byte
- * for length information.
- *
- * Item blowup is trickier to measure.	Suppose the "copy" op copies
- * 4 bytes of data.  Because of a special check in the encoding code,
- * we produce a 4-byte copy only if the offset is < 65536.	Therefore
- * the copy op takes 3 bytes to encode, and this type of item leads
- * to at most the 62/60 blowup for representing literals.
- *
- * Suppose the "copy" op copies 5 bytes of data.  If the offset is big
- * enough, it will take 5 bytes to encode the copy op.	Therefore the
- * worst case here is a one-byte literal followed by a five-byte copy.
- * I.e., 6 bytes of input turn into 7 bytes of "compressed" data.
- *
- * This last factor dominates the blowup, so the final estimate is:
- */
+/* customized variant of memcpy, which can overwrite up to 8 bytes beyond dstEnd */
+static void LZ4_wildCopy8(void* dstPtr, const void* srcPtr, void* dstEnd)
+{
+    BYTE* d = (BYTE*)dstPtr;
+    const BYTE* s = (const BYTE*)srcPtr;
+    BYTE* const e = (BYTE*)dstEnd;
+
+    do { LZ4_memcpy(d,s,8); d+=8; s+=8; } while (d<e);
+}
+
+
+static void LZ4_writeLE16(void* memPtr, U16 value)
+{
+    if (LZ4_isLittleEndian()) {
+        LZ4_write16(memPtr, value);
+    } else {
+        BYTE* p = (BYTE*)memPtr;
+        p[0] = (BYTE) value;
+        p[1] = (BYTE)(value>>8);
+    }
+}
+
+/*! LZ4_compressBound() :
+    Provides the maximum size that LZ4 compression may output in a "worst case" scenario (input data not compressible)
+    This function is primarily useful for memory allocation purposes (destination buffer size).
+    Note that LZ4_compress_default() compresses faster when dstCapacity is >= LZ4_compressBound(srcSize)
+        inputSize  : max supported value is LZ4_MAX_FILE_LENGTH
+        return : maximum output size in a "worst case" scenario
+              or 0, if input size is incorrect (too large or negative)
+*/
 static inline uint32_t snappy_max_compressed_length(uint32_t input_length) {
 	if (input_length > 0) 
-		return (32 + input_length + input_length / 6);
+		return input_length + ((input_length)/255) + 16;
 	else
 		return 0;
 }
@@ -232,84 +288,6 @@ static inline int32_t find_match_length(uint8_t *s1, uint8_t *s2, uint8_t *s2_li
 	return matched;
 }
 
-/**
- * Emit a literal element.
- *
- * @param output: holds output buffer information
- * @param literal: buffer storing the literal data
- * @param len: length of the literal
- */
-static void emit_literal(struct host_buffer_context *output, uint8_t *literal, uint32_t len)
-{
-	//printf("emit_literal %d %d\n", len, output->curr-output->buffer);
-	uint32_t n = len - 1; // Zero-length literals are disallowed
-	
-	if (n < 60) {
-		*output->curr++ = EL_TYPE_LITERAL | (n << 2);
-	}
-	else {
-		uint8_t *base = output->curr;
-		uint8_t count = 0;
-		output->curr++;
-		while (n > 0) {
-			*output->curr++ = n & 0xFF;
-			n >>= 8;
-			count++;
-		}
-
-		*base = EL_TYPE_LITERAL | ((59 + count) << 2);
-	}
-	
-	memcpy(output->curr, literal, len);
-	output->curr += len;
-}
-
-/**
- * Emit a copy element that is less than 64-bytes in length.
- *
- * @param output: holds output buffer information
- * @param offset: offset of the copy
- * @param len: length of the copy
- */
-static void emit_copy_less_than64(struct host_buffer_context *output, uint32_t offset, uint32_t len)
-{
-	if ((len < 12) && (offset < 2048)) {
-		*output->curr++ = EL_TYPE_COPY_1 + ((len - 4) << 2) + ((offset >> 8) << 5);
-		*output->curr++ = offset & 0xFF;
-	}
-	else {
-		*output->curr++ = EL_TYPE_COPY_2 + ((len - 1) << 2);
-		*output->curr++ = offset & 0xFF;
-		*output->curr++ = (offset >> 8) & 0xFF;
-	}
-}
-
-/**
- * Emit copy elements in chunks of length 64-bytes.
- *
- * @param output: holds output buffer information
- * @param offset: offset of the copy
- * @param len: length of the copy
- */
-static void emit_copy(struct host_buffer_context *output, uint32_t offset, uint32_t len) 
-{
-	//printf("emit_copy %d %d %d\n", offset, len, output->curr - output->buffer);
-	
-	// Emit 64-byte copies but keep at least four bytes reserved
-	while (len >= 68) {
-		emit_copy_less_than64(output, offset, 64);
-		len -= 64;
-	}
-
-	// Emit an extra 60-byte copy if we have too much data to fit in one copy
-	if (len > 64) {
-		emit_copy_less_than64(output, offset, 60);
-		len -= 60;
-	}
-
-	// Emit remainder
-	emit_copy_less_than64(output, offset, len);
-}
 
 /**
  * Perform Snappy compression on a block of input data, and save the compressed
@@ -323,136 +301,137 @@ static void emit_copy(struct host_buffer_context *output, uint32_t offset, uint3
  */
 static void compress_block(struct host_buffer_context *input, struct host_buffer_context *output, uint32_t input_size, uint16_t *table, uint32_t table_size)
 {
-	uint8_t *base_input = input->curr;
-	uint8_t *input_end = input->curr + input_size;
 	const int32_t shift = 32 - log2_floor(table_size);
+	BYTE* ip = (BYTE*) input->curr;		
+	BYTE* op = (BYTE*) output->curr;
+	const BYTE* base = (const BYTE*) input -> curr;	
 
-	// Make space for compressed length
-	output->curr += 4;
-	uint8_t *output_start = output->curr;
 
-	/*
-	 * Bytes in [next_emit, input->curr) will be emitted as literal bytes.
-	 * Or [next_emit, input_end) after the main loop.
-	 */
-	uint8_t *next_emit = input->curr;
-	const uint32_t input_margin_bytes = 15;
+	const BYTE* lowLimit = (const BYTE*) input-> curr;
+	const BYTE* const iend = ip + input_size;	
+	const BYTE* const mflimitPlusOne = iend - MFLIMIT + 1;
+	const BYTE* anchor = (const BYTE*) ip;
 
-	if (input_size >= input_margin_bytes) {
-		const uint8_t *const input_limit = input->curr + input_size - input_margin_bytes;
-		
-		uint32_t next_hash;
-		for (next_hash = hash(++input->curr, shift);;) {
-			/*
-			 * The body of this loop calls EmitLiteral once and then EmitCopy one or
-			 * more times.	(The exception is that when we're close to exhausting
-			 * the input we goto emit_remainder.)
-			 *
-			 * In the first iteration of this loop we're just starting, so
-			 * there's nothing to copy, so calling EmitLiteral once is
-			 * necessary.  And we only start a new iteration when the
-			 * current iteration has determined that a call to EmitLiteral will
-			 * precede the next call to EmitCopy (if any).
-			 *
-			 * Step 1: Scan forward in the input looking for a 4-byte-long match.
-			 * If we get close to exhausting the input then goto emit_remainder.
-			 *
-			 * Heuristic match skipping: If 32 bytes are scanned with no matches
-			 * found, start looking only at every other byte. If 32 more bytes are
-			 * scanned, look at every third byte, etc.. When a match is found,
-			 * immediately go back to looking at every byte. This is a small loss
-			 * (~5% performance, ~0.1% density) for lcompressible data due to more
-			 * bookkeeping, but for non-compressible data (such as JPEG) it's a huge
-			 * win since the compressor quickly "realizes" the data is incompressible
-			 * and doesn't bother looking for matches everywhere.
-			 *
-			 * The "skip" variable keeps track of how many bytes there are since the
-			 * last match; dividing it by 32 (ie. right-shifting by five) gives the
-			 * number of bytes to move ahead for each iteration.
-			 */
-			uint32_t skip_bytes = 32;
-			uint8_t *next_input = input->curr;
-			uint8_t *candidate;
-			do {
-				input->curr = next_input;
-				uint32_t hval = next_hash;
-				uint32_t bytes_between_hash_lookups = skip_bytes++ >> 5;
-				next_input = input->curr + bytes_between_hash_lookups;
+	//TODO: Add checks for input of size 0, input < input_margin_bytes...
 
-				if (next_input > input_limit)
-					goto emit_remainder;
+	if (input_size < MFLIMIT + 1) goto _last_literals;
 
-				next_hash = hash(next_input, shift);
-				candidate = base_input + table[hval];
-				table[hval] = input->curr - base_input;
-			} while (read_uint32(input->curr) != read_uint32(candidate));
+	/* First Byte */
+	table[hash(ip, shift)] = ip - base;
+	U32 forwardH = hash(++ip, shift);
+	
+	for ( ; ; ) {
+		BYTE* match;
+		BYTE* token;
+
+		BYTE* forwardIp = ip;
+		int step = 1;
+		int searchMatchNb = 1 << LZ4_skipTrigger;
+		do {
+			U32 h = forwardH;
+			ip = forwardIp;
+			forwardIp += step;
+			step = (searchMatchNb++ >> LZ4_skipTrigger);
+
+			if (forwardIp > mflimitPlusOne) goto _last_literals;
+			assert(ip < mflimitPlusOne);
+
+			match = base + table[h];
+			forwardH = hash(forwardIp, shift);		
+			table[h] = forwardIp - base;
+		} while (read_uint32(match) != read_uint32(ip));
+
+
+		while (((ip>anchor) & (match > lowLimit)) && (ip[-1]==match[-1])) { ip--; match--; }
+
+		/* Encode Literals*/
+		{	unsigned const litLength = (unsigned) (ip - anchor);
+			token = op++;
 			
-			/*
-			 * Step 2: A 4-byte match has been found.  We'll later see if more
-			 * than 4 bytes match.	But, prior to the match, input bytes
-			 * [next_emit, input->curr) are unmatched.	Emit them as "literal bytes."
-			 */
-			emit_literal(output, next_emit, input->curr - next_emit);
+			if (litLength >= RUN_MASK) {
+				int len = (int) (litLength - RUN_MASK);
+				*token = (RUN_MASK<<ML_BITS);
+				for(; len >= 255 ; len-=255) *op++ = 255;
+				*op++ = (BYTE) len;
+			}
+			else *token = (BYTE)(litLength<<ML_BITS);
 
-			/*
-			 * Step 3: Call EmitCopy, and then see if another EmitCopy could
-			 * be our next move.  Repeat until we find no match for the
-			 * input immediately after what was consumed by the last EmitCopy call.
-			 *
-			 * If we exit this loop normally then we need to call EmitLiteral next,
-			 * though we don't yet know how big the literal will be.  We handle that
-			 * by proceeding to the next iteration of the main loop.  We also can exit
-			 * this loop via goto if we get close to exhausting the input.
-			 */
-			uint8_t *insert_tail;
-			uint32_t candidate_bytes = 0;
-
-			do {
-				/*
-				 * We have a 4-byte match at input->curr, and no need to emit any
-				 *	"literal bytes" prior to input->curr.
-				 */
-				const uint8_t *base = input->curr;
-				int32_t matched = 4 + find_match_length(candidate + 4, input->curr + 4, input_end);
-				input->curr += matched;
-
-				int32_t offset = base - candidate;
-				emit_copy(output, offset, matched);
-			
-				/*
-				 * We could immediately start working at input->curr now, but to improve
-				 * compression we first update table[Hash(input->curr - 1, ...)]/
-				 */
-				insert_tail = input->curr - 1;
-				next_emit = input->curr;
-				if (input->curr >= input_limit)
-					goto emit_remainder;
-
-				uint32_t prev_hash = hash(insert_tail, shift);
-				table[prev_hash] = input->curr - base_input - 1;
-
-				uint32_t curr_hash = hash(insert_tail + 1, shift);
-				candidate = base_input + table[curr_hash];
-				candidate_bytes = read_uint32(candidate);
-				table[curr_hash] = input->curr - base_input;
-			} while(read_uint32(insert_tail + 1) == candidate_bytes);
-
-			next_hash = hash(insert_tail + 2, shift);
-			input->curr++;
+			/* Copy Literals */
+			LZ4_wildCopy8(op, anchor, op+litLength);
+			op+=litLength;
 		}
-	}
-				
-emit_remainder:
-	/* Emit the remaining bytes as literal */
-	if (next_emit < input_end) {
-		emit_literal(output, next_emit, input_end - next_emit);
-		input->curr = input_end;
+
+_next_match:
+		/* at this stage, the following variables must be correctly set :
+         * - ip : at start of LZ operation
+         * - match : at start of previous pattern occurrence; can be within current prefix, or within extDict
+         * - offset : if maybe_ext_memSegment==1 (constant)
+         * - lowLimit : must be == dictionary to mean "match is within extDict"; must be == source otherwise
+         * - token and *token : position to write 4-bits for match length; higher 4-bits for literal length supposed already written
+    	*/
+
+		/* Encode Offset */
+		assert(ip-match <= LZ4_DISTANCE_MAX);
+		LZ4_writeLE16(op, (U16)(ip - match)); op+=2;
+
+		/* Encode MatchLength */
+		{	unsigned matchCode;
+
+			if (matchCode >= ML_MASK) {
+				*token += ML_MASK;
+				matchCode -= ML_MASK;
+				LZ4_write32(op, 0xFFFFFFFF);
+			
+				while (matchCode >= 4*255) {
+					op+=4;
+					LZ4_write32(op, 0xFFFFFFFF);
+					matchCode -= 4*255;
+				}
+				op += matchCode / 255;
+				*op++ = (BYTE)(matchCode % 255);
+			} else
+				*token += (BYTE)(matchCode);
+		}
+		
+
+		anchor = ip;
+
+		/* Test end of chunk */
+		if (ip >= mflimitPlusOne) break;
+
+		/* Fill table */
+		table[hash(ip-2, shift)] = (ip-2) - base;
+
+		/* Test next position */
+		match = base + table[hash(ip, shift)];
+		table[hash(ip, shift)] = ip - base;
+		if ( (match+LZ4_DISTANCE_MAX >= ip) 
+		&& (read_uint32(match) == read_uint32(ip)) )
+		{ token=op++; *token=0; goto _next_match; }
+
+		/* Prepare next loop */
+		forwardH = hash(++ip, shift);
 	}
 
-	write_uint32(output_start - 4, output->curr - output_start);
+_last_literals:
+	/* Encode Last Literals */
+
+	{	size_t lastRun = (size_t)(iend - anchor);
+		if (lastRun >= RUN_MASK) {
+			size_t accumulator = lastRun - RUN_MASK;
+			*op++ = RUN_MASK << ML_BITS;
+			for(; accumulator >= 255 ; accumulator -=255) *op++ = 255;
+			*op++ = (BYTE) accumulator;
+		} else {
+			*op++ = (BYTE)(lastRun<<ML_BITS);
+		}
+		LZ4_memcpy(op, anchor, lastRun);
+		ip = anchor + lastRun;
+		op += lastRun;	
+	}
+
 }
-
-
+			
 /*************** Public Functions *******************/
 
 void setup_compression(struct host_buffer_context *input, struct host_buffer_context *output, struct program_runtime *runtime) 
@@ -461,28 +440,6 @@ void setup_compression(struct host_buffer_context *input, struct host_buffer_con
 	struct timeval end;
 	gettimeofday(&start, NULL);
 
-	/*
-	 * Compressed data can be defined as:
-	 *	  compressed := item* literal*
-	 *	  item		 := literal* copy
-	 *
-	 * The trailing literal sequence has a space blowup of at most 62/60
-	 * since a literal of length 60 needs one tag byte + one extra byte
-	 * for length information.
-	 *
-	 * Item blowup is trickier to measure.	Suppose the "copy" op copies
-	 * 4 bytes of data.  Because of a special check in the encoding code,
-	 * we produce a 4-byte copy only if the offset is < 65536.	Therefore
-	 * the copy op takes 3 bytes to encode, and this type of item leads
-	 * to at most the 62/60 blowup for representing literals.
-	 *
-	 * Suppose the "copy" op copies 5 bytes of data.  If the offset is big
-	 * enough, it will take 5 bytes to encode the copy op.	Therefore the
-	 * worst case here is a one-byte literal followed by a five-byte copy.
-	 * I.e., 6 bytes of input turn into 7 bytes of "compressed" data.
-	 *
-	 * This last factor dominates the blowup, so the final estimate is:
-	 */
 	uint32_t max_compressed_length = snappy_max_compressed_length(input->length);
 	output->buffer = malloc(sizeof(uint8_t) * max_compressed_length);
 	output->curr = output->buffer;
@@ -499,10 +456,10 @@ snappy_status snappy_compress_host(struct host_buffer_context *input, struct hos
 
 	// Write the decompressed length
 	uint32_t length_remain = input->length;
-	write_varint32(output, length_remain);
+	//write_varint32(output, length_remain);
 
 	// Write the decompressed block size
-	write_varint32(output, block_size);
+	//write_varint32(output, block_size);
 
 	while (input->curr < (input->buffer + input->length)) {
 		// Get the next block size ot compress
